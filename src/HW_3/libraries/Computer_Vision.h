9e37236d88454c84301b850a573ab02bef589cc6
Kevin Eppacher <kevin-eppacher@hotmail.de>
2024-04-28 17:24:32 +0000

    Aufgeräumt

---
+++ src/HW_3/libraries/Computer_Vision.h
@@ -0,0 +1,21 @@
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <opencv2/xfeatures2d.hpp>
+
+namespace ComputerVision
+{
+    class FeatureExtraction
+    {
+    private:
+        
+        
+    public:
+        FeatureExtraction()
+        {
+            std::cout<<"FeatureExtraction Class was created"<<std::endl;
+        }
+        ~FeatureExtraction(){}
+    };
+    
+} // namespace ComputerVision
+
---
+++ src/HW_3/src/LearnObject/feature_extraction.cpp
@@ -0,0 +1,12 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/xfeatures2d.hpp>
+#include <fstream>
+
+int main() 
+{
+    cv::Mat imgSIFT, imgSIFT2;
+
+
+
+    return 0;
+}
